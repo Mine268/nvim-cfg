e78482d85c2a047f1b7ff7ac0aea401b2cf4489e
mine268 <mine268@foxmail.com>
2026-04-28 12:26:45 +0800

    给键位添加了desc描述字段

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,77 +1,68 @@
--- define common options
 local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
+	noremap = true,
+	silent = true,
 }
 
 -----------------
 --- Tabs move ---
 -----------------
-vim.keymap.set('n', '<leader>[', ':tabprevious<CR>', opts)
-vim.keymap.set('n', '<leader>]', ':tabnext<CR>', opts)
+vim.keymap.set('n', '<leader>[', ':tabprevious<CR>', vim.tbl_extend('force', opts, { desc = 'Previous tab' }))
+vim.keymap.set('n', '<leader>]', ':tabnext<CR>',     vim.tbl_extend('force', opts, { desc = 'Next tab' }))
 
 -----------------
 --- Line move ---
 -----------------
-vim.keymap.set('n', '<S-J>', ':m .+1<CR>==', opts)
-vim.keymap.set('n', '<S-K>', ':m .-2<CR>==', opts)
-vim.keymap.set('v', '<S-J>', ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set('v', '<S-K>', ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set('n', '<S-J>', ':m .+1<CR>==',    vim.tbl_extend('force', opts, { desc = 'Move line down' }))
+vim.keymap.set('n', '<S-K>', ':m .-2<CR>==',    vim.tbl_extend('force', opts, { desc = 'Move line up' }))
+vim.keymap.set('v', '<S-J>', ":m '>+1<CR>gv=gv", vim.tbl_extend('force', opts, { desc = 'Move selection down' }))
+vim.keymap.set('v', '<S-K>', ":m '<-2<CR>gv=gv", vim.tbl_extend('force', opts, { desc = 'Move selection up' }))
 
 -----------------
 -- Normal mode --
 -----------------
--- Alt + 方向键缩放窗口
-vim.keymap.set('n', '<A-Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<A-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<A-Up>',    ':resize -2<CR>',         vim.tbl_extend('force', opts, { desc = 'Shrink window height' }))
+vim.keymap.set('n', '<A-Down>',  ':resize +2<CR>',         vim.tbl_extend('force', opts, { desc = 'Expand window height' }))
+vim.keymap.set('n', '<A-Left>',  ':vertical resize -2<CR>', vim.tbl_extend('force', opts, { desc = 'Shrink window width' }))
+vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>', vim.tbl_extend('force', opts, { desc = 'Expand window width' }))
 
--- Alt + hjkl 切换窗口（normal 模式）
-vim.keymap.set('n', '<A-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<A-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<A-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<A-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<A-h>', '<C-w>h', vim.tbl_extend('force', opts, { desc = 'Focus left window' }))
+vim.keymap.set('n', '<A-j>', '<C-w>j', vim.tbl_extend('force', opts, { desc = 'Focus window below' }))
+vim.keymap.set('n', '<A-k>', '<C-w>k', vim.tbl_extend('force', opts, { desc = 'Focus window above' }))
+vim.keymap.set('n', '<A-l>', '<C-w>l', vim.tbl_extend('force', opts, { desc = 'Focus right window' }))
 
 -----------------
 -- Visual mode --
 -----------------
--- Hint: start visual mode with the same area as the previous area and the same mode
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('v', '<', '<gv', vim.tbl_extend('force', opts, { desc = 'Decrease indent, keep selection' }))
+vim.keymap.set('v', '>', '>gv', vim.tbl_extend('force', opts, { desc = 'Increase indent, keep selection' }))
 
 -----------------
 --- Terminal  ---
 -----------------
--- 终端模式下按 Esc 退出终端模式（进入 normal 模式）
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
--- 终端模式下用 Alt+方向键直接切换窗口
-vim.keymap.set('t', '<A-k>', [[<C-\><C-n><C-w>k]], opts)
-vim.keymap.set('t', '<A-j>', [[<C-\><C-n><C-w>j]], opts)
-vim.keymap.set('t', '<A-h>', [[<C-\><C-n><C-w>h]], opts)
-vim.keymap.set('t', '<A-l>', [[<C-\><C-n><C-w>l]], opts)
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]],     vim.tbl_extend('force', opts, { desc = 'Exit terminal mode' }))
+vim.keymap.set('t', '<A-k>', [[<C-\><C-n><C-w>k]], vim.tbl_extend('force', opts, { desc = 'Focus window above' }))
+vim.keymap.set('t', '<A-j>', [[<C-\><C-n><C-w>j]], vim.tbl_extend('force', opts, { desc = 'Focus window below' }))
+vim.keymap.set('t', '<A-h>', [[<C-\><C-n><C-w>h]], vim.tbl_extend('force', opts, { desc = 'Focus left window' }))
+vim.keymap.set('t', '<A-l>', [[<C-\><C-n><C-w>l]], vim.tbl_extend('force', opts, { desc = 'Focus right window' }))
 
--- VS Code Neovim 模式下跳过插件相关键位
+-- VS Code Neovim skips plugin-related keymaps below
 if not vim.g.vscode then
 
 -----------------
 --- LSP docs  ---
 -----------------
--- gh: 快速查看光标下符号的文档（与 0.12 默认的 K 键功能相同，多一个选择）
-vim.keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "LSP hover docs" })
--- gs: 查看当前函数的签名
+vim.keymap.set("n", "gh", vim.lsp.buf.hover,          { desc = "LSP hover docs" })
 vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { desc = "LSP signature help" })
 
 -----------------
 --- Telescope ---
 -----------------
--- Reserved keymaps; will be bound automatically when telescope is installed
 local ok, telescope_builtin = pcall(require, 'telescope.builtin')
 if ok then
-    vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+	vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Find files' })
+	vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep,  { desc = 'Live grep' })
+	vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers,    { desc = 'Buffers' })
+	vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags,  { desc = 'Help tags' })
 end
 
 end
